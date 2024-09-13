068c1ddfa75f279123eefeb0170f6bfb9696a751
Abdelrahman Elsharkawy <95210189+Abdelrahmansaied@users.noreply.github.com>
2024-09-13 11:37:22 +0300

    Update html_pdf.py

--- html_pdf.py
+++ html_pdf.py
@@ -65,7 +65,7 @@ if uploaded_file:
 
         if st.button("Convert"):
             if urls:
-                save_path = r"C:\report"
+                save_path = "output_pdfs"  # This directory should exist in your repo
                 with st.spinner("Converting..."):
                     convert_urls_to_pdfs(urls, mpns, save_path)
                     st.success("Conversion completed! PDFs saved in the specified path.")
