d718be9d6eff5246c6a89f8af9e4ca0588694423
Abdelrahman Elsharkawy <95210189+Abdelrahmansaied@users.noreply.github.com>
2024-09-13 11:19:01 +0300

    Update html_pdf.py

--- html_pdf.py
+++ html_pdf.py
@@ -3,6 +3,7 @@ import time
 import os
 import pandas as pd
 from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from PIL import Image
