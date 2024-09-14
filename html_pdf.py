49a1a50f22e5fbc889e9fa10daac6c34c4214665
Abdelrahman Elsharkawy <95210189+Abdelrahmansaied@users.noreply.github.com>
2024-09-14 14:35:56 +0300

    Update html_pdf.py

--- html_pdf.py
+++ html_pdf.py
@@ -44,6 +44,8 @@ def convert_urls_to_pdfs(urls, mpns):
     options.add_argument('--disable-dev-shm-usage')
     options.add_argument('--no-sandbox')
     options.add_argument('--ignore-certificate-errors')
+    options.add_argument('--disable-web-security')
+    options.add_argument('--allow-running-insecure-content')
 
     pdf_buffers = []
     with tempfile.TemporaryDirectory() as temp_dir:
