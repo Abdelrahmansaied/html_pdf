c71cea048e8617cc3c19c46522936c75165a7469
Abdelrahman Elsharkawy <95210189+Abdelrahmansaied@users.noreply.github.com>
2024-09-22 15:28:02 +0300

    Update html_pdf.py

--- html_pdf.py
+++ html_pdf.py
@@ -88,9 +88,7 @@ def convert_urls_to_pdfs(urls, mpns, additional_text, output_dir):
     options.add_argument("--window-size=1920,1080")
     options.add_argument('--disable-gpu')
     options.add_argument('--ignore-certificate-errors')
-
-    user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36"
-    options.add_argument(f"user-agent={user_agent}")
+    options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36")
     options.add_argument('--lang=en')
         
     pdf_paths = []
